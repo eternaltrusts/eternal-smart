4954e388f913560f606d7bc8b7fbbdea7f37c07e
Gorshenev <goralexgen@gmail.com>
2018-06-10 11:38:23 +0300

    code review
    code review

--- simpletrusts_mvp.sol
+++ simpletrusts_mvp.sol
@@ -1,10 +1,6 @@
 pragma solidity ^0.4.21;
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
+
 contract Ownable {
 
     address public owner;
