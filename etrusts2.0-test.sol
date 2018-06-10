19f9fe3eb67cbbd2413cbb491607c0e46cd8e03e
Gorshenev <goralexgen@gmail.com>
2018-06-10 11:23:42 +0300

    code review
    code review

--- etrusts2.0-test.sol
+++ etrusts2.0-test.sol
@@ -1,40 +1,5 @@
 pragma solidity ^0.4.21;
 
-/*
- * @title String & slice utility library for Solidity contracts.
- * @author Nick Johnson <arachnid@notdot.net>
- *
- * @dev Functionality in this library is largely implemented using an
- *      abstraction called a 'slice'. A slice represents a part of a string -
- *      anything from the entire string to a single character, or even no
- *      characters at all (a 0-length slice). Since a slice only has to specify
- *      an offset and a length, copying and manipulating slices is a lot less
- *      expensive than copying and manipulating the strings they reference.
- *
- *      To further reduce gas costs, most functions on slice that need to return
- *      a slice modify the original one instead of allocating a new one; for
- *      instance, `s.split(".")` will return the text up to the first '.',
- *      modifying s to only contain the remainder of the string after the '.'.
- *      In situations where you do not want to modify the original slice, you
- *      can make a copy first with `.copy()`, for example:
- *      `s.copy().split(".")`. Try and avoid using this idiom in loops; since
- *      Solidity has no memory management, it will result in allocating many
- *      short-lived slices that are later discarded.
- *
- *      Functions that return two slices come in two versions: a non-allocating
- *      version that takes the second slice as an argument, modifying it in
- *      place, and an allocating version that allocates and returns the second
- *      slice; see `nextRune` for example.
- *
- *      Functions that have to copy string data will return strings rather than
- *      slices; these can be cast back to slices for further processing if
- *      required.
- *
- *      For convenience, some functions are provided with non-modifying
- *      variants that create a new slice and return both; for instance,
- *      `s.splitNew('.')` leaves s unmodified, and returns two values
- *      corresponding to the left and right parts of the string.
- */
 
 library strings {
     struct slice {
@@ -43,7 +8,7 @@ library strings {
     }
 
     function memcpy(uint dest, uint src, uint len) private pure {
-        // Copy word-length chunks while possible
+
         for(; len >= 32; len -= 32) {
             assembly {
                 mstore(dest, mload(src))
@@ -61,11 +26,7 @@ library strings {
         }
     }
 
-    /*
-     * @dev Returns a slice containing the entire string.
-     * @param self The string to make a slice from.
-     * @return A newly allocated slice containing the entire string.
-     */
+
     function toSlice(string self) internal pure returns (slice) {
         uint ptr;
         assembly {
@@ -74,11 +35,6 @@ library strings {
         return slice(bytes(self).length, ptr);
     }
 
-    /*
-     * @dev Returns the length of a null-terminated bytes32 string.
-     * @param self The value to find the length of.
-     * @return The length of the string, from 0 to 32.
-     */
     function len(bytes32 self) internal pure returns (uint) {
         uint ret;
         if (self == 0)
@@ -105,13 +61,6 @@ library strings {
         return 32 - ret;
     }
 
-    /*
-     * @dev Returns a slice containing the entire bytes32, interpreted as a
-     *      null-termintaed utf-8 string.
-     * @param self The bytes32 value to convert to a slice.
-     * @return A new slice containing the value of the input argument up to the
-     *         first null.
-     */
     function toSliceB32(bytes32 self) internal pure returns (slice ret) {
         // Allocate space for `self` in memory, copy it there, and point ret at it
         assembly {
@@ -146,14 +95,7 @@ library strings {
         return ret;
     }
 
-    /*
-     * @dev Returns the length in runes of the slice. Note that this operation
-     *      takes time proportional to the length of the slice; avoid using it
-     *      in loops, and call `slice.empty()` if you only need to know whether
-     *      the slice is empty or not.
-     * @param self The slice to operate on.
-     * @return The length of the slice in runes.
-     */
+
     function len(slice self) internal pure returns (uint l) {
         // Starting at ptr-31 means the LSB will be the byte we care about
         uint ptr = self._ptr - 31;
@@ -186,15 +128,7 @@ library strings {
         return self._len == 0;
     }
 
-    /*
-     * @dev Returns a positive number if `other` comes lexicographically after
-     *      `self`, a negative number if it comes before, or zero if the
-     *      contents of the two slices are equal. Comparison is done per-rune,
-     *      on unicode codepoints.
-     * @param self The first slice to compare.
-     * @param other The second slice to compare.
-     * @return The result of the comparison.
-     */
+
     function compare(slice self, slice other) internal pure returns (int) {
         uint shortest = self._len;
         if (other._len < self._len)
@@ -427,13 +361,7 @@ library strings {
         return equal;
     }
 
-    /*
-     * @dev If `self` ends with `needle`, `needle` is removed from the
-     *      end of `self`. Otherwise, `self` is unmodified.
-     * @param self The slice to operate on.
-     * @param needle The slice to search for.
-     * @return `self`
-     */
+
     function until(slice self, slice needle) internal pure returns (slice) {
         if (self._len < needle._len) {
             return self;
@@ -554,14 +482,7 @@ library strings {
         return self;
     }
 
-    /*
-     * @dev Modifies `self` to contain the part of the string from the start of
-     *      `self` to the end of the first occurrence of `needle`. If `needle`
-     *      is not found, `self` is set to the empty slice.
-     * @param self The slice to search and modify.
-     * @param needle The text to search for.
-     * @return `self`.
-     */
+
     function rfind(slice self, slice needle) internal pure returns (slice) {
         uint ptr = rfindPtr(self._len, self._ptr, needle._len, needle._ptr);
         self._len = ptr - self._ptr;
