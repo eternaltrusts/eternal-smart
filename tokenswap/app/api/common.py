35be23692dad8dd4bd457ccd3099b4de7d6a356e
Gorshenev <goralexgen@gmail.com>
2018-06-11 11:11:16 +0300

    correct version
    correct version

--- tokenswap/app/api/common.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask_restful import Resource
-
-class CommonResource(Resource):
-    def __init__(self, **kwargs):
-        # smart_engine is a black box dependency
-        self.models = kwargs['models']
-        self.utils = kwargs['utils']
